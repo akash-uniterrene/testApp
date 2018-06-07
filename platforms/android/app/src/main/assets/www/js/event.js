809943239b8d5b6e941ab618600188394e6c3184
akash-uniterrene <akash@uniterrene.com>
2018-06-07 18:44:53 +0530

    new commit

---
+++ platforms/android/app/src/main/assets/www/js/event.js
@@ -0,0 +1,27 @@
+
+
+function onPause() {
+    // Handle the pause event
+}
+
+function onResume() {
+    // Handle the resume event
+}
+
+function onMenuKeyDown() {
+    // Handle the menubutton event
+}
+
+function onVolumeKeyUp() {
+    // Handle the menubutton event
+	alert("BATTERY STATUS:  Level: " + info.level + " isPlugged: " + info.isPlugged);
+}
+
+function onVolumeKeyDown() {
+    // Handle the menubutton event
+}
+
+function onBatteryStatus() {
+    // Handle the menubutton event
+	 alert("BATTERY STATUS:  Level: " + info.level + " isPlugged: " + info.isPlugged);
+}
